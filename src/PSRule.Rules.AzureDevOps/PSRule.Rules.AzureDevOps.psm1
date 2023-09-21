7636fdc7996c0b406d7de5cc4f2eb8bdaa009524
Roderick Bant <13252390+webtonize@users.noreply.github.com>
2023-09-21 19:30:23 +0200

    Add organization level export

--- src/PSRule.Rules.AzureDevOps/PSRule.Rules.AzureDevOps.psm1
+++ src/PSRule.Rules.AzureDevOps/PSRule.Rules.AzureDevOps.psm1
@@ -55,5 +55,51 @@ Function Export-AzDevOpsRuleData {
     Export-AzDevOpsVariableGroups -PAT $PAT -Organization $Organization -Project $Project -OutputPath $OutputPath
     Export-AzDevOpsReleaseDefinitions -PAT $PAT -Organization $Organization -Project $Project -OutputPath $OutputPath
 }
-Export-ModuleMember -Function Export-AzDevOpsRuleData
+Export-ModuleMember -Function Export-AzDevOpsProjectRuleData -Alias Export-AzDevOpsRuleData
 # End of Function Export-AzDevOpsRuleData
+
+<#
+    .SYNOPSIS
+    Export rule data for all projects in the DevOps organization
+
+    .DESCRIPTION
+    Export rule data for all projects in the DevOps organization using Azure DevOps Rest API and this modules functions for analysis by PSRule
+
+    .PARAMETER PAT
+    Personal Access Token (PAT) for Azure DevOps
+
+    .PARAMETER Organization
+    Organization name for Azure DevOps
+
+    .PARAMETER OutputPath
+    Output path for JSON files
+
+    .EXAMPLE
+    Export-AzDevOpsOrganizationRuleData -PAT $PAT -Organization $Organization -OutputPath $OutputPath
+#>
+Function Export-AzDevOpsOrganizationRuleData {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [string]
+        $PAT,
+        [Parameter()]
+        [string]
+        $Organization,
+        [Parameter()]
+        [string]
+        $OutputPath
+    )
+    $projects = Get-AzDevOpsProjects -PAT $PAT -Organization $Organization
+    $projects | ForEach-Object {
+        $project = $_
+        # Create a subfolder for each project
+        $subPath = "$OutputPath\$($project.name)"
+        if(!(Test-Path -Path $subPath)) {
+            New-Item -Path $subPath -ItemType Directory
+        }
+        Export-AzDevOpsRuleData -PAT $PAT -Organization $Organization -Project $project.name -OutputPath $subPath
+    }
+}
+Export-ModuleMember -Function Export-AzDevOpsOrganizationRuleData
+# End of Function Export-AzDevOpsOrganizationRuleData
